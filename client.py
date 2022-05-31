ca95e1427e2582a2cb438534d6ba5687c2b9613e
vkarinje <vkarinje@pdx.edu>
2022-05-31 16:22:41 -0700

    initial push

---
+++ client.py
@@ -0,0 +1,419 @@
+import os
+import sys
+import time
+import ast
+import _thread
+from _thread import *
+import socket
+from socket import *
+import struct
+from struct import *
+#import simplecrypt
+#from simplecrypt import encrypt, decrypt
+
+flag = True
+subscription_list = []
+file_count = 0
+decryption_key = ''
+uid = ''
+
+def exp_handle(sock):
+	print('The IRC Server is not responding at this time. Try connecting after some time')
+	quit_routine(sock)
+	exit()
+
+####################################################################################################
+
+def error(code):
+	if code == 'NE001':
+		return 'IrcNameError001: Enter the username without any spaces \nType the username again'
+	elif code == 'NE002':
+		return 'IrcNameError002: A username cannot be empty. Type atleast one character'
+	elif code == 'NE003':
+		return 'IrcNameError003: The maximum length allowed is 20 for the username'
+	elif code == 'NE004':
+		return 'IrcNameError004: The maximum length allowed for roomname is 10'
+	elif code == 'AE001':
+		return 'IrcArgumentError001: Too many arguments typed'
+	elif code == 'AE002':
+		return 'IrcArgumentError002: Too few arguments given'
+	elif code == "AE004":
+		return 'IrcArgumentError004: Invalid value of argument/s for given command.Type the right argument'
+	elif code == 'ME001':
+		return 'IrcMessageError001: Unauthorized message\nYou do not have the authorization for posting this message'
+	elif code == 'ME002':
+		return 'IrcMessageError002: Message size is bigger than the size permitted'
+	elif code == 'CE001':
+		return 'IrcCommandError001: Please enter a valid command.'
+	elif code == 'CE002':
+		return 'IrcCommandError002: This command can only be executed once for given set of arguments'
+	elif code == 'IOE01':
+		return 'IrcIOError01: IRC could not open file\nPlease check the file name and make sure it is in your current working directory'
+	elif code == 'NEEMP':
+		return "IrcCommandError002: Username cannot be empty"
+####################################################################################################
+
+def initializer(handler_socket):
+	global uid
+	print(handler_socket.recv(64).decode('ascii'))
+	uid_status = 'unregistered'
+	while uid_status == 'unregistered':
+		uid = input('Enter a username: ')
+		if len(uid)==0:
+			print(error('NEEMP'))
+		if len(uid.split(' ')) > 1:
+			print(error('NE001'))
+		elif len(uid.split(' ')) < 1:
+			print(error('NE002'))
+		elif len(uid.split(' ')) == 1:
+			if len(uid) > 20:
+				print(error('NE003'))
+			else:
+				try:
+					handler_socket.send(bytes('register '+uid, 'utf-8'))
+					uid_status = handler_socket.recv(12).decode('ascii')
+					print(handler_socket.recv(128).decode('ascii'))
+				except ConnectionError:
+			#	except OSError:
+					exp_handle(handler_socket)
+	print('\nTo see the commands type \"help\" \n')
+	server_handler(handler_socket, uid)
+
+####################################################################################################
+
+def quit_routine(sock):
+	sock.close()
+	global flag
+	flag = False
+	return
+
+def message_routine(handler_socket, command):
+	header = ' '.join(command[0:2])
+	message = ' '.join(command[2:])
+	if len(message) > 448:
+		print(error('ME002'))
+		if input('\nSend anyway ? Y/N') == (N or n):
+			return
+	try:
+		handler_socket.send(bytes(header, 'utf-8'))
+		handler_socket.send(bytes(message, 'utf-8'))
+	except ConnectionError:
+#	except OSError:
+		exp_handle(handler_socket)
+	else:
+		return
+
+def list_routine(handler_socket, command):
+	try:
+		handler_socket.send(bytes(command, 'utf-8'))
+		buff = handler_socket.recv(4)
+	#	buff = handler_socket.recv(8)
+	except ConnectionError:
+#	except OSError:
+		exp_handle(handler_socket)
+	else:
+		size = unpack('L', buff)[0]
+	try:
+		string = handler_socket.recv(size).decode('ascii')
+	except ConnectionError:
+#	except OSError:
+		exp_handle(handler_socket)
+	else:
+		List = ast.literal_eval(string)
+		print('\n\t'+command.split(' ')[1]+': ', str(List)[1:-1])
+	return
+
+def transfer_routine(handler_socket, command):
+	try:
+		fh = open(command[1], 'rb')
+		File = fh.read()
+		fh.close()
+	except IOError:
+		print(error('IOE01'))
+	else:
+		try:
+			handler_socket.send(bytes(' '.join(command), 'utf-8'))
+			trans_status1 = handler_socket.recv(10).decode('ascii')
+			if trans_status1 == 'Failed':
+				return
+			key = input(handler_socket.recv(64).decode('ascii'))
+			handler_socket.send(bytes(key, 'utf-8'))
+			trans_status2 = handler_socket.recv(10).decode('ascii')
+		except ConnectionError:
+	#	except OSError:
+			exp_handle(handler_socket)
+		else:
+			if trans_status2 == 'Success':
+				print('Key Verified: Preparing to send file\n')
+				handler_socket.send(pack('L', sys.getsizeof(File)))
+				handler_socket.send(File)
+				print('Sending the file...\n')
+			else:
+				print('Key not verified: Not authorized to send file to '+command[2])
+	return
+
+def secure_routine(handler_socket, command):
+	cryptext = encrypt(command[2], ' '.join(command[3:]))
+	try:
+		handler_socket.send(bytes(' '.join(command[0:2]), 'utf-8'))
+		handler_socket.send(pack('L', sys.getsizeof(cryptext)))
+		handler_socket.send(cryptext)
+	except ConnectionError:
+#	except OSError:
+		exp_handle(handler_socket)
+	return
+
+####################################################################################################
+
+def help():
+	print("\nTo create a new room or to join an existing room:\n	join-room					Usage: 	join-room <room-name>\n\nTo exit from a room:\n	exit-room					Usage: 	exit-room <room-name>\n\nSend message to a room:\n	chat-room					Usage: 	chat-room <room-name> <your message here>\n\nTo send a private message to another user within same or different room:\n	pvt-msg						Usage: 	pvt-msg <recipient-name> <your message here>\n\nTo send a secure message to another user:\n	secure-msg					Usage:	secure-msg <recipient-name> <key> <your message here>\n\nTo enter decryption key:\n	decryption-key					Usage:	decryption-key <key>\n\nList all the available rooms, active users, or members of a room:\n	list						Usage: 	list rooms / list users / list members <room-name>\n\nList all the rooms subscribed by the user:\n	my-rooms					Usage: 	my-rooms\n\nTo set the file transfer key for receiving files:\n	set-file-transfer-key				Usage: 	set-file-transfer-key <key>\n\nTo send a file to another user:\n	send-file					Usage: 	send-file <file-name.extension> <recipient-name>\n\nBroadcast message to all active users:\n	broadcast					Usage:	broadcast all <your message here>\n\nTo quit IRC:\n	quit-irc					Usage: 	quit-irc\n\nTo view this message:\n	help						Usage:	help")
+
+####################################################################################################
+
+def server_handler(handler_socket, uid):
+	while True:
+		command = input('%s:\n' %(uid))
+		cmd_split = command.split(' ')
+		intent = cmd_split[0]
+		if intent == 'quit-irc':
+			if len(cmd_split) > 1:
+				print(error('AE001')+'\nUsage: quit-irc')
+			else:
+				try:
+					handler_socket.send(bytes(command, 'utf-8'))
+				except ConnectionError:
+			#	except OSError:
+					exp_handle(handler_socket)
+				else:
+					quit_routine(handler_socket)
+				finally:
+					sys.exit()
+
+		elif intent == 'join-room':
+			if len(cmd_split) < 2 or len(cmd_split[1].strip()) == 0:
+				print(error('AE002')+'\nUsage: join-room <room-name>')
+			elif len(cmd_split) > 2:
+				print(error('AE001')+'\nUsage: join-room <room-name>')
+			elif len(cmd_split) == 2:
+				if len(cmd_split[1]) > 10:
+					print(error('NE004'))
+				else:
+					if cmd_split[1] not in subscription_list:
+						try:
+							handler_socket.send(bytes(command, 'utf-8'))
+							print(handler_socket.recv(64).decode('ascii'))
+						except ConnectionError:
+					#	except OSError:
+							exp_handle(handler_socket)
+						else:
+							subscription_list.append(cmd_split[1])
+					else:
+						print(error('CE002'))
+
+		elif intent == 'exit-room':
+			if len(cmd_split) < 2:
+				print(error('AE002')+'\nUsage: exit-room <room-name>')
+			elif len(cmd_split) > 2:
+				print(error('AE001')+'\nUsage: exit-room <room-name>')
+			elif len(cmd_split) == 2:
+				if cmd_split[1] in subscription_list:
+					subscription_list.remove(cmd_split[1])
+					try:
+						handler_socket.send(bytes(command, 'utf-8'))
+					except ConnectionError:
+				#	except OSError:
+						exp_handle(handler_socket)
+				else:
+					print(error("AE004"))
+
+		elif intent == 'chat-room':
+			if len(cmd_split) < 3:
+				print(error('AE002')+'\nUsage: chat-room <room-name> <your message here>')
+			else:
+				if cmd_split[1] in subscription_list:
+					message_routine(handler_socket, cmd_split)
+				else:
+					print(error('ME001')+'\nPlease join the room to send messages')
+
+		elif intent == 'pvt-msg':
+			if len(cmd_split) < 3:
+				print(error('AE002')+'\nUsage: pvt-msg <recipient-name> <your message here>')
+			else:
+				message_routine(handler_socket, cmd_split)
+
+		elif intent == 'secure-msg':
+			if len(cmd_split) < 4:
+				print(error('AE002')+'\nUsage: secure-msg <recipient-name> <key> <your message here>')
+			else:
+				message = ' '.join(cmd_split[3:])
+				print('\nSecure Messaging is only available on linux servers. The secure message code has been commented out so as to run the project on Windows host. To test secure messaging on linux, the required code could be un-commented as directed at the top of the source code.\n')
+			#	secure_routine(handler_socket, cmd_split)
+
+		elif intent == 'list':
+			if len(cmd_split) > 3:
+				print(error('AE001') + '\nUsage: list rooms/users OR list members <room-name>')
+			elif len(cmd_split) < 2:
+				print(error('AE002') + '\nUsage: list rooms/users OR list members <room-name>')
+			elif cmd_split[1] == 'members':
+				if len(cmd_split) < 3:
+					print(error('AE002') + '\nUsage: list members <room-name>')
+				else:
+					list_routine(handler_socket, command)
+			elif cmd_split[1] == 'rooms' or 'users':
+				if len(cmd_split) > 2:
+					print(error('AE001') + '\nUsage: list rooms/users')
+				else:
+					list_routine(handler_socket, command)
+			else:
+				print(error('AE004'))
+
+		elif intent == 'my-rooms':
+			if len(cmd_split) > 1:
+				print(error('AE001') + '\nUsage: my-rooms')
+			else:
+				print('\n\tMy Rooms: ', str(subscription_list)[1:-1])
+
+		elif intent == 'set-file-transfer-key':
+			if len(cmd_split) < 2:
+				print(error('AE002') + '\nUsage: set-file-transfer-key <key>')
+			else:
+				if len(' '.join(cmd_split[1:])) > 10:
+					print('The maximum key size allowed is 10 characters\n Enter the correct key')
+				else:
+					try:
+						handler_socket.send(bytes(command, 'utf-8'))
+						print(handler_socket.recv(64).decode('ascii'))
+					except ConnectionError:
+				#	except OSError:
+						exp_handle(handler_socket)
+
+		elif intent == 'send-file':
+			if len(cmd_split) > 3:
+				print(error('AE001') + '\nUsage: send-file <file-name.extension> <recipient-name>')
+			elif len(cmd_split) < 3:
+				print(error('AE002') + '\nUsage: send-file <file-name.extension> <recipient-name>')
+			else:
+				transfer_routine(handler_socket, cmd_split)
+
+		elif intent == 'broadcast':
+			if len(cmd_split) < 3:
+				print(error('AE002') + '\nUsage: broadcast all <your message here>')
+			else:
+				message_routine(handler_socket, cmd_split)
+
+		elif intent == 'decryption-key':
+			global decryption_key
+			decryption_key = cmd_split[1]
+
+		elif intent == 'help':
+			if len(cmd_split) > 1:
+				print(error('AE001') + '\nUsage: help')
+			else:
+				help()
+		else:
+			print(error('CE001'))
+
+####################################################################################################
+
+def message_handler(message_socket):
+	global flag
+	global decryption_key
+	global uid
+	while flag:
+		connection_m, address_m = message_socket.accept()
+		try:
+			buff = connection_m.recv(512).decode('ascii')
+			if buff.split(' ')[0] == 'secure-msg':
+				sender = buff.split(' ')[1]
+				connection_m.send(b'ok')
+				size = unpack('L', connection_m.recv(4))[0]
+			#	size = unpack('L', connection_m.recv(8))[0]
+				sec_buff = connection_m.recv(size)
+				print('Received secure message from '+sender+'. To view the message enter the decryption key')
+				while not decryption_key:
+					time.sleep(2)
+				message = decrypt(decryption_key, sec_buff).decode('ascii')
+				print(sender+' says: '+message+'\n'+uid+':\n')
+				decryption_key = ''
+			else:
+				print(buff)
+
+		except ConnectionError:
+	#	except OSError:
+			exp_handle(message_socket)
+		else:
+			connection_m.close()
+	_thread.exit()
+
+####################################################################################################
+
+def file_handler(file_socket):
+	global flag
+	while flag:
+		connection_f, address_f = file_socket.accept()
+		try:
+			head_size = unpack('L', connection_f.recv(4))[0]
+		#	head_size = unpack('L', connection_f.recv(8))[0]
+			header = connection_f.recv(head_size).decode('ascii')
+			sender, ext = tuple(header.split(' '))
+			file_size = unpack('L', connection_f.recv(4))[0]
+		#	file_size = unpack('L', connection_f.recv(8))[0]
+			File = connection_f.recv(file_size)
+		except ConnectionError:
+	#	except OSError:
+			exp_handle(file_socket)
+		else:
+			global file_count
+			file_name = 'file'+str(file_count)+'.'+ext
+			fh = open(file_name, 'w+b')
+			fh.write(File)
+			fh.close()
+			file_count += 1
+			global uid
+			print('You received file '+file_name+' from '+sender+'\n'+uid+':')
+			connection_f.close()
+	_thread.exit()
+
+####################################################################################################
+
+server_socket = socket()
+try:
+	server_socket.connect((gethostname(), 1234))
+	buff = server_socket.recv(4)
+#	buff = server_socket.recv(8)
+except ConnectionError:
+#except OSError:
+	exp_handle(server_socket)
+
+port_h = unpack('L', buff)[0]
+server_socket.close()
+
+handler_socket = socket()
+try:
+	handler_socket.connect((gethostname(), port_h))
+except ConnectionError:
+#except OSError:
+	exp_handle(handler_socket)
+
+port_m = port_h + 1
+port_f = port_h + 2
+
+message_socket = socket()
+try:
+	message_socket.bind((gethostname(), port_m))
+	message_socket.listen()
+except ConnectionError:
+#except OSError:
+	exp_handle(handler_socket)
+
+file_socket = socket()
+try:
+	file_socket.bind((gethostname(), port_f))
+	file_socket.listen()
+except ConnectionError:
+#except OSError:
+	exp_handle(handler_socket)
+
+start_new_thread(message_handler, (message_socket,))
+start_new_thread(file_handler, (file_socket,))
+initializer(handler_socket)
---
+++ server.py
@@ -0,0 +1,339 @@
+import os
+import time
+import _thread
+from _thread import *
+import socket
+from socket import *
+import struct
+from struct import *
+
+list_lock = _thread.allocate_lock()
+user_list = {}
+hallway = {}
+file_transfer_keys = {}
+port_map = {}
+
+port_h = 2050
+
+
+def initializer(handler_socket, port_h):
+    connection_h, address_h = handler_socket.accept()
+    connection_h.send(b'\nWelcome to Internet Relay Chat')
+    status = 'unregistered'
+    global port_m
+    while status == 'unregistered':
+        try:
+            command = connection_h.recv(32).decode('ascii')
+        except ConnectionError:
+            #	except OSError:
+            _thread.exit()
+        else:
+            [intent, argument] = command.split(' ')
+            if intent == 'register':
+                list_lock.acquire()
+                if argument not in user_list:
+                    user_list[argument] = port_m
+                    list_lock.release()
+                    port_map[port_m] = argument
+                    status = 'registered'
+                    connection_h.send(bytes(status, 'utf-8'))
+                    connection_h.send(b'\nRegistered Successfully')
+                else:
+                    list_lock.release()
+                    connection_h.send(bytes(status, 'utf-8'))
+                    connection_h.send(
+                        b'\nIrcRegError001: This username already exists.\nPlease try with a different username')
+            elif intent != 'register':
+                connection_h.send(b'\nPlease register with a username to proceed')
+        print(user_list)
+        client_handler(connection_h, port_m)
+
+
+####################################################################################################
+
+def get_temp_socket(member, temp_port):
+    sock = socket()
+    try:
+        sock.connect((gethostname(), temp_port))
+    except ConnectionError:
+        #	except OSError:
+        exp_handle(temp_port)
+    return sock
+
+
+def send_msg(msg, member):
+    message_socket = get_temp_socket(member, user_list[member])
+    try:
+        message_socket.send(bytes(msg, 'utf-8'))
+    except ConnectionError:
+        #	except OSError:
+        exp_handle(user_list[member])
+    if msg.split(' ')[0] == 'secure-msg':
+        return message_socket
+
+    message_socket.close()
+    return
+
+
+def quit_routine(connection_h, uid):
+    connection_h.close()
+    time.sleep(2)
+    del_list = []
+    for room_members in hallway.values():
+        if uid in room_members:
+            room_members.remove(uid)
+    if uid in user_list:
+        del user_list[uid]
+    if uid in file_transfer_keys:
+        del file_transfer_keys[uid]
+    for user in user_list.keys():
+        msg = '\nUser ' + uid + ' left IRC\n' + user + ': '
+        send_msg(msg, user)
+    for room, member_list in hallway.items():
+        if len(member_list) == 0:
+            del_list.append(room)
+    for room in del_list:
+        del hallway[room]
+    return
+
+
+def join_room_routine(connection_h, room, uid):
+    if room not in hallway:
+        hallway[room] = [uid]
+        connection_h.send(b'\nYou joined a newly created room')
+    else:
+        for member in hallway[room]:
+            msg = '\nUser ' + uid + ' joined room ' + room + '\n' + member + ': '
+            send_msg(msg, member)
+        hallway[room].append(uid)
+        connection_h.send(b'\nYou joined an existing room')
+
+    print(hallway)
+
+
+def exit_routine(room, uid):
+    hallway[room].remove(uid)
+    if len(hallway[room]) == 0:
+        del hallway[room]
+    else:
+        for member in hallway[room]:
+            msg = '\nUser ' + uid + ' left room ' + room + '\n' + member + ': '
+            send_msg(msg, member)
+    return
+
+
+def chat_room_routine(room, user, message):
+    msg = '\n' + user + ' @ ' + room + ' says: ' + message
+    room = hallway[room][:]
+    room.remove(user)
+    for member in room:
+        send_msg(msg + '\n' + member + ': ', member)
+    return
+
+
+def list_routine(connection_h, List):
+    List = str(list(List))
+    connection_h.send(pack('L', len(List)))
+    connection_h.send(bytes(List, 'utf-8'))
+    return
+
+
+def exp_handle(port_m):
+    del_list = []
+    if port_m in port_map:
+        uid = port_map[port_m]
+        del port_map[port_m]
+    else:
+        return
+    if uid in file_transfer_keys:
+        del file_transfer_keys[uid]
+    for room_members in hallway.values():
+        if uid in room_members:
+            room_members.remove(uid)
+    if uid in user_list:
+        del user_list[uid]
+    for user in user_list.keys():
+        msg = '\nUser ' + uid + ' left IRC\n' + user + ': '
+        send_msg(msg, user)
+    for room, member_list in hallway.items():
+        if len(member_list) == 0:
+            del_list.append(room)
+    for room in del_list:
+        del hallway[room]
+    return
+
+
+def transfer_routine(sender, receiver, ext, size, File):
+    try:
+        file_socket = get_temp_socket(receiver, user_list[receiver] + 1)
+        file_socket.send(pack('L', len(' '.join([sender, ext]))))
+        file_socket.send(bytes(' '.join([sender, ext]), 'utf-8'))
+        file_socket.send(pack('L', size))
+        file_socket.send(File)
+    except ConnectionError:
+        #	except OSError:
+        exp_handle(user_list[receiver])
+        send_msg('Could not send file to ' + receiver + ' due to Connection Error\n' + sender + ': ', sender)
+
+
+def secure_routine(sender, receiver, message, size):
+    try:
+        message_socket = send_msg('secure-msg ' + sender, receiver)
+        message_socket.recv(2)
+        message_socket.send(pack('L', size))
+        message_socket.send(message)
+        message_socket.close()
+    except ConnectionError:
+        #	except OSError:
+        exp_handle(user_list[receiver])
+        send_msg('Could not send secure message to ' + receiver + ' due to Connection Error\n' + sender + ': ', sender)
+
+
+####################################################################################################
+
+def client_handler(connection_h, port_m):
+    uid = port_map[port_m]
+    while True:
+        try:
+            command = connection_h.recv(128).decode('ascii')
+            command = command.split(' ')
+        except ConnectionError:
+            #	except OSError:
+            exp_handle(port_m)
+            _thread.exit()
+        else:
+            intent = command[0]
+
+        if intent == 'quit-irc':
+            send_msg('\nThank You for using IRC\nGoodbye!', uid)
+            quit_routine(connection_h, uid)
+            break
+
+        elif intent == 'join-room':
+            join_room_routine(connection_h, command[1], uid)
+
+        elif intent == 'exit-room':
+            exit_routine(command[1], uid)
+
+        elif intent == 'chat-room':
+            try:
+                message = connection_h.recv(448).decode('ascii')
+            except ConnectionError:
+                #	except OSError:
+                exp_handle(port_m)
+                _thread.exit()
+            else:
+                chat_room_routine(command[1], uid, message)
+
+        elif intent == 'pvt-msg':
+            try:
+                message = connection_h.recv(448).decode('ascii')
+            except ConnectionError:
+                #	except OSError:
+                exp_handle(port_m)
+                _thread.exit()
+            else:
+                member = command[1]
+                if member not in user_list:
+                    msg = '\nIrcArgumentError003: Username ' + member + ' does not exist\n' + uid + ': '
+                    send_msg(msg, uid)
+                else:
+                    msg = '\n' + uid + ' says: ' + message + '\n' + uid + ':'
+                    send_msg(msg, command[1])
+
+        elif intent == 'secure-msg':
+            try:
+                size = unpack('L', connection_h.recv(4))[0]
+                #	size = unpack('L', connection_h.recv(8))[0]
+                message = connection_h.recv(size)
+            except ConnectionError:
+                #	except OSError:
+                exp_handle(port_m)
+                _thread.exit()
+            else:
+                member = command[1]
+                if member not in user_list:
+                    msg = '\nIrcArgumentError003: Username ' + member + ' does not exist\n' + uid + ': '
+                    send_msg(msg, uid)
+                else:
+                    secure_routine(uid, member, message, size)
+
+        elif intent == 'list':
+            if command[1] == 'rooms':
+                if len(hallway) == 0:
+                    msg = '\nNo rooms to display\n'
+                    send_msg(msg, uid)
+                list_routine(connection_h, hallway.keys())
+            elif command[1] == 'users':
+                list_routine(connection_h, user_list.keys())
+            elif command[1] == 'members':
+                if command[2] not in hallway:
+                    msg = '\nIrcArgumentError003: Room ' + command[2] + ' does not exist\n' + command[-1] + ': '
+                    send_msg(msg, uid)
+                else:
+                    list_routine(connection_h, hallway[command[2]])
+
+        elif intent == 'set-file-transfer-key':
+            file_transfer_keys[uid] = ' '.join(command[1:])
+            connection_h.send(b'Your file transfer key was successfully set\n')
+            print(file_transfer_keys)
+
+        elif intent == 'send-file':
+            member = command[2]
+            if member not in user_list:
+                msg = '\nIrcArgumentError003: Username ' + member + ' does not exist\n' + uid + ': '
+                send_msg(msg, uid)
+                connection_h.send(b'Failed')
+            elif member not in file_transfer_keys:
+                msg = 'IrcFileTransferError001: ' + member + ' has not set a file transfer key\n'
+                send_msg(msg, uid)
+                connection_h.send(b'Failed')
+            else:
+                try:
+                    connection_h.send(b'Not Failed')
+                    connection_h.send(bytes('Enter ' + member + '\'s file transfer key: ', 'utf-8'))
+                    key = connection_h.recv(16).decode('ascii')
+                except ConnectionError:
+                    #	except OSError:
+                    exp_handle(port_m)
+                if key == file_transfer_keys[member]:
+                    connection_h.send(b'Success')
+                    try:
+                        size = unpack('L', connection_h.recv(4))[0]
+                        #	size = unpack('L', connection_h.recv(8))[0]
+                        File = connection_h.recv(size)
+                    except ConnectionError:
+                        #	except OSError:
+                        exp_handle(user_list[sender])
+                    ext = command[1].split('.')[-1]
+                    start_new_thread(transfer_routine, (uid, member, ext, size, File))
+
+                else:
+                    connection_h.send(b'Failed')
+
+        elif intent == 'broadcast':
+            msg = uid + ' says: ' + connection_h.recv(448).decode('ascii')
+            for user in user_list.keys():
+                if user != uid:
+                    send_msg(msg + '\n' + user + ': ', user)
+    return
+
+
+####################################################################################################
+
+server_socket = socket()
+server_socket.bind((gethostname(), 1234))
+print('\nIRC server is listening . . .')
+server_socket.listen()
+while True:
+    connection, address = server_socket.accept()
+    port_m = port_h + 1
+    port_f = port_h + 2
+
+    handler_socket = socket()
+    handler_socket.bind((gethostname(), port_h))
+    connection.send(pack('L', port_h))
+    handler_socket.listen()
+    start_new_thread(initializer, (handler_socket, port_h,))
+
+    port_h += 5
